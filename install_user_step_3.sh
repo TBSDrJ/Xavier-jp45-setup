cb94245898df9415cbb49b50b1275458b868b801
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2022-03-21 17:20:12 -0700

    Update LD_PRELOAD statements, they are needed

--- install_user_step_3.sh
+++ install_user_step_3.sh
@@ -1,23 +1,15 @@
 ###### STEP 3, RUN THIS AS USER
 
-# 2022-03-22 Gonna try without this on the first pass, see if we need it.
 # Needed by Tensorflow to get it to properly find/allocate GPU
-# echo 'export LD_PRELOAD=$LD_PRELOAD:/usr/lib/aarch64-linux-gnu/libgomp.so.1' >> /home/student/.bashrc
+echo 'export LD_PRELOAD=$LD_PRELOAD:/usr/lib/aarch64-linux-gnu/libgomp.so.1' >> /home/student/.bashrc
 
 # scikit-learn and scikit-image function better as user installs.
 # There was some cache that wasn't useable if it was sudo installed
 pip3 install scikit-learn scikit-image
 
-# 2022-03-22 See what happens with this also
 # This is needed by scikit-learn; I wasn't sure why a different libgomp was needed when we've
 #   already pre-loaded one for Tensorflow, but it mattered in my tests.
-# echo 'export LD_PRELOAD=$LD_PRELOAD:/home/student/.local/lib/python3.6/site-packages/scikit_learn.libs/libgomp-d22c30c5.so.1.0.0' >> /home/student/.bashrc
-
-# 2022-03-22 This is now in the tensorflow prerequisites so should be covered in step 2.
-# Dependency for Pytorch
-# Notice that instructions call for installing numpy but we already have that updated
-#   from Tensorflow install.
-# pip3 install Cython
+echo 'export LD_PRELOAD=$LD_PRELOAD:/home/student/.local/lib/python3.6/site-packages/scikit_learn.libs/libgomp-d22c30c5.so.1.0.0' >> /home/student/.bashrc
 
 # Install Pytorch
 pip3 install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
