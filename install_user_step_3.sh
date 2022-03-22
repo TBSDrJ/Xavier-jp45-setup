20b5ade4797f48b2c18e4b4a180d77a71f40c335
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2022-03-21 17:28:18 -0700

    Update pip calls and add matplotlib upgrade

--- install_user_step_3.sh
+++ install_user_step_3.sh
@@ -5,14 +5,17 @@ echo 'export LD_PRELOAD=$LD_PRELOAD:/usr/lib/aarch64-linux-gnu/libgomp.so.1' >> 
 
 # scikit-learn and scikit-image function better as user installs.
 # There was some cache that wasn't useable if it was sudo installed
-pip3 install scikit-learn scikit-image
+python3 -m pip install scikit-learn scikit-image
+
+# Upgrade matplotlib, version was 2.1.1 without upgrade, get 3.3.4 with upgrade.
+python3 -m pip install -U matplotlib
 
 # This is needed by scikit-learn; I wasn't sure why a different libgomp was needed when we've
 #   already pre-loaded one for Tensorflow, but it mattered in my tests.
 echo 'export LD_PRELOAD=$LD_PRELOAD:/home/student/.local/lib/python3.6/site-packages/scikit_learn.libs/libgomp-d22c30c5.so.1.0.0' >> /home/student/.bashrc
 
 # Install Pytorch
-pip3 install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+python3 -m pip install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
 
 # Environment variable for Torchvision setup.
 export BUILD_VERSION=0.11.0
@@ -26,4 +29,4 @@ mv ~/tensorflow-2.5.0+nv21.8-cp36-cp36m-linux_aarch64.whl ~/Downloads
 mv ~/torch-1.10.0-cp36-cp36m-linux_aarch64.whl ~/Downloads
 
 # Install Dynamixel SDK
-pip3 install dynamixel-sdk
+python3 -m pip install dynamixel-sdk
